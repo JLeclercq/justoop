bf060246c354403b0682b1d715e8da2d99c9a113
Oreste Notelli <oreste.notelli@plecno.com>
2015-02-28 19:15:09 +0100

    grunt fixed

--- justoop/Gruntfile.js
+++ justoop/Gruntfile.js
@@ -4,11 +4,16 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             options: {
-                separator: ';'
+                separator: ';',
+                banner:"/*  <%= pkg.name %> <%= pkg.version%> . Lightweight Object Oriented Library For Javascript */\n"
             },
             dist: {
                 src: ['src/justoop.js'],
                 dest: 'dist/<%= pkg.name %>.js'
+            },
+            with_underscore: {
+                src: ['src/justoop.js', 'node_modules/underscore/underscore.js'],
+                dest: 'dist/<%= pkg.name %>-with-underscore.js'
             }
         },
         uglify: {
@@ -16,28 +21,31 @@ module.exports = function (grunt) {
                 sourceMap: true,
                 mangle:["eval"],
                 reserved:["f_"],
-                banner: '/*! <%= pkg.name %>-r <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+                banner: '/*! <%= pkg.name %> <%= pkg.version%> -r <%= grunt.template.today("dd-mm-yyyy") %> */\n'
             },
 	    dist: {
                 files: {
-                    'dist/<%= pkg.name %>.min.js': ["<%= concat.dist.dest %>"]
+                    'dist/<%= pkg.name %>.<%= pkg.version%>.min.js': ["<%= concat.dist.dest %>"],
+                    'dist/<%= pkg.name %>.<%= pkg.version%>-with-underscore.min.js': ["<%= concat.with_underscore.dest %>"]
                 }
             }
-        },
+        }
+        /*,
         browserify: {
             options:{
                 exclude:["external"]
             },
             dist: {
-                src: ['src/**/*.js'],
-                dest: 'dist/<%= pkg.name %>-browser.js'
+                src: ['src/justoop.js'],
+                dest: 'dist/<%= pkg.name %>-with-underscore.js'
             }
             
-        }
+        }*/
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    //grunt.loadNpmTasks('grunt-browserify');
     //grunt.registerTask('test', [ ]);
 
     grunt.registerTask('default', ['concat',  'uglify',]);
